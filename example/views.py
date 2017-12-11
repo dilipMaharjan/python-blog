732a3f5395cf42a7e9f8a3ec748a23f3cbebb78b
crazdrms <dilipmaharjann@gmail.com>
2017-12-10 20:33:48 -0500

    passing data from context

--- example/views.py
+++ example/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     template='home.html'
-    context={}
+    context={'title':'home','message':'Hello from context'}
     return render(request,template,context)
