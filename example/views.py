ea8b23630187e223c13d31757288d96ace57ad51
crazdrms <dilipmaharjann@gmail.com>
2017-12-10 20:38:46 -0500

    quering user data using request.GET.get('key')

--- example/views.py
+++ example/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
+    query=request.GET.get('name')
     template='home.html'
-    context={'title':'home','message':'Hello from context'}
+    context={'title':'home','message':'Hello, {} from context'.format(query)}
     return render(request,template,context)
