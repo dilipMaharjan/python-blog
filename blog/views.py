17c652dbb0e377e8d75067e8623118c078e45b69
crazdrms <dilipmaharjann@gmail.com>
2017-12-19 22:46:27 -0500

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+()
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Post
+
+
+# class based list view
+class PostListView(ListView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListView, self).get_context_data(**kwargs)
+        context['object_list'] = Post.objects.filter(status='Published')
+        return context
+
+
+# function based list view
+def post_list(request):
+    template = 'blog/post_list.html'
+    objects_list = Post.objects.filter(status='Published')
+    context = {
+        'objects_list': objects_list,
+    }
+    return render(request, template, context)
+
+
+# class based detail view
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        return context
+
+
+# function based list view
+def post_detail(request, slug):
+    template = 'blog/post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    context = {
+        'post': post,
+    }
+    return render(request, template, context)
