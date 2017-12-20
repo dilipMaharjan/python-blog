95b0adb265a470ecaa5e4fc64425d1accdd24bbc
crazdrms <dilipmaharjann@gmail.com>
2017-12-19 21:04:51 -0500

    Post,Category,superuser,admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Post
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+    list_display = ('title', 'slug', 'category', 'created', 'updated')
+    list_filter = ('status',)
+    search_fields = ('title', 'body')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
