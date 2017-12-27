f66191f314f94534cea2f9398caf9220fb7a5108
crazdrms <dilipmaharjann@gmail.com>
2017-12-26 20:45:53 -0500

    category list,url

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import PostListView, PostDetailView, post_list, post_detail
+from .views import PostListView, PostDetailView, post_list, post_detail, category_detail
 
 urlpatterns = [
-    url(r'^blog-list/$', PostListView.as_view(), name='list'),
-    url(r'^(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='detail'),
-    url(r'^blog-f-detail/$', post_list, name='list'),
-    url(r'^(?P<slug>[-\w]+)/$', post_detail, name='detail'),
+    url(r'^post-list/$', PostListView.as_view(), name='post_list'),
+    url(r'^post-detail/(?P<slug>[-\w]+)/$', PostDetailView.as_view(), name='post_detail'),
+    url(r'^post-f-detail/$', post_list, name='list'),
+    url(r'^post-f-detail/(?P<slug>[-\w]+)/$', post_detail, name='detail'),
+    url(r'^category-detail/(?P<slug>[-\w]+)/$', category_detail, name='category_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-
-()
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Post
+from .models import Post, Category
 
 
 # class based list view
@@ -14,6 +12,15 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(PostListView, self).get_context_data(**kwargs)
         context['object_list'] = Post.objects.filter(status='Published')
+        return context
+
+
+# class based detail view
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
         return context
 
 
@@ -27,20 +34,24 @@ def post_list(request):
     return render(request, template, context)
 
 
-# class based detail view
-class PostDetailView(DetailView):
-    model = Post
-
-    def get_context_data(self, **kwargs):
-        context = super(PostDetailView, self).get_context_data(**kwargs)
-        return context
-
-
 # function based list view
 def post_detail(request, slug):
     template = 'blog/post_detail.html'
     post = get_object_or_404(Post, slug=slug)
     context = {
         'post': post,
+    }
+    return render(request, template, context)
+
+
+# function based list view
+def category_detail(request, slug):
+    template = 'category/category_detail.html'
+
+    category = get_object_or_404(Category, slug=slug)
+    posts = Post.objects.filter(category=category)
+    context = {
+        'category': category,
+        'posts': posts
     }
     return render(request, template, context)
