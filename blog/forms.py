408b34120701ae16ed46cb5ed441ff45f4f7e490
crazdrms <dilipmaharjann@gmail.com>
2017-12-27 23:11:58 -0500

    styling form

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=['title','body','category','seo_title','seo_description','status']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'seo_title': forms.TextInput(attrs={'class': 'form-control'}),
+            'seo_description': forms.TextInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
